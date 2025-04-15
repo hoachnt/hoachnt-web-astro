8eab36d8fd601ed1dfbef1d39a88fc8cc5b7ed34
hoachnt <hoachprogrammer@gmail.com>
2025-04-15 12:02:40 +0700

    Update deisgn of PostRow.astro

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,11 +1,11 @@
 export const LINKS = {
-  github: "https://github.com/hoachnt",
-  linkedin: "https://www.linkedin.com/in/hoachnt/",
-  email: "hoachprogrammer@gmail.com<",
+	github: "https://github.com/hoachnt",
+	linkedin: "https://www.linkedin.com/in/hoachnt/",
+	email: "hoachprogrammer@gmail.com<",
 };
 
 export const loaderAnimation = [
-  ".loader",
-  { opacity: [1, 0], pointerEvents: "none" },
-  { easing: "ease-out" },
+	".loader",
+	{ opacity: [1, 0], pointerEvents: "none" },
+	{ easing: "ease-out" },
 ];
